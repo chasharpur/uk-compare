695325b629c9139b94e69e8e791245e9fa886e1b
Desmond Schmidt <desmond.allan.schmidt@gmail.com>
2026-07-25 21:32:37 +1000

    added update script

---
+++ update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+declare -a m_files=()
+declare -a a_files=()
+while read -r line
+do
+    flag=(`awk '{print $1}' <<< "$line"`)
+    if [ "$flag" == '??' ]; then
+        a_files+=(`awk '{print $2}' <<< "$line"`)
+    else
+        m_files+=(`awk '{print $2}' <<< "$line"`)
+    fi
+done < <(git status -s)
+files_to_add=""
+for f in ${a_files[@]}; do
+    files_to_add="$files_to_add $f"
+done
+files_to_commit=""
+for f in ${m_iles[@]}; do
+    files_to_commit="$files_to_commit $f"
+done
+if [ ! -z "$files_to_add" ]; then
+    git add "$files_to_add"
+fi
+if [ ! -z "$files_to_add" ] && [ ! -z "$files_to_commit" ]; then
+    scp "$files_to_commit $files_to_add" charles-harpur.org:/var/www/uk-compare/
+fi
+read -p "git message: " message
+git commit -a -m "$message"
